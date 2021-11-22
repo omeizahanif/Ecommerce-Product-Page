c0a40f919979712e7b2bd44c03e3b2532a74026e
Hanif Adams <omeizahanif@gmail.com>
2021-11-22 18:36:24 +0100

    added functionality for lightbox

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const body = document.querySelector("body");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const slides = document.querySelectorAll(".slides");
+const lightboxSlides = document.querySelectorAll(".lightbox-slides");
 const slideNavigation = document.querySelectorAll(".slide-navigation img");
+const lightboxNavigation = document.querySelectorAll(".lightbox-navigation img");
+
 let slideIndex = 1;
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -38,9 +41,21 @@ function showSlide(n) {
     //hide all slides
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+        lightboxSlides[i].style.display = "none";
         slideNavigation[i].classList.remove("active");
+        lightboxNavigation[i].classList.remove("active");
     }
     //display only selected slide
     slides[slideIndex - 1].style.display = "block";
+    lightboxSlides[slideIndex - 1].style.display = "block";
     slideNavigation[slideIndex - 1].classList.add("active");
+    lightboxNavigation[slideIndex - 1].classList.add("active");
+}
+
+function openLightbox() {
+    document.getElementById("lightbox").style.display = "block";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
 }
